987f2533bbe32fd133a33fc9fd3bdac557fce37a
Benjamin Gomori <benjamingomori@gmail>
2020-06-19 13:56:58 +0300

    Added developmet js component - CartIconCounter

---
+++ src/components/cart/CartIconCounter.js
@@ -0,0 +1,41 @@
+export default class CartIconCounter {
+	constructor() {
+		this.element = document.querySelector('.cart-item-counter');
+		this.numElement = document.querySelector('.cart-item-counter__num');
+		this._currentVal = 0;
+	}
+
+	// setValue(val) {
+	// 	this.numElement.innerHTML = null; //TODO
+	// 	this.numElement.innerHTML = '' + val; //TODO
+	// 	this._currentVal = +val;
+	// 	this.shouldDisplayOrNot();
+	// }
+
+	adjustValue(amount) {
+		this.numElement.innerHTML = null; //TODO
+		this._currentVal += +amount;
+		this.numElement.innerHTML = '' + this._currentVal;
+		this.shouldDisplayOrNot();
+	}
+
+	getValue(val) {
+		return this._currentVal;
+	}
+
+	_displayCounter = () => {
+		this.element.style.visibility = 'visible';
+	};
+	_hideCounter = () => {
+		this.element.style.visibility = 'hidden';
+	};
+
+	shouldDisplayOrNot() {
+		if (this._currentVal <= 0) {
+			this._hideCounter();
+			return;
+		}
+
+		this._displayCounter();
+	}
+}
